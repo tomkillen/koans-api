c452d31070c1dda6724875510aa358051be5925e
Tom Killen <tomkillen@gmail.com>
2024-06-19 12:56:24 +0200

    Made implicit dependency on expect explicit

--- typings/express/index.d.ts
+++ typings/express/index.d.ts
@@ -5,11 +5,15 @@ import UserService from "../../src/services/user/user.service"
 
 declare global {
   namespace Express {
+    // We inject these dependencies into express.app.locals
+    // so provide typings here that indicate these values WILL be defined
     export interface Application {
       userService: UserService;
       authService: AuthService;
       activityService: ActivityService;
     }
+    // Some middleware creates these values in express.response.locals
+    // so provide typings here that indicate these values MAY be defined
     export interface Locals {
       accessToken?: string;
       user?: {
